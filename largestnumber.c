10817c28bdb5a02ea46aaf3ed30d633c751e3ac2
Vaibhav Raj <Vaibhavraj049@gmail.com>
2024-09-27 12:12:14 +0530

    Add files via upload

---
+++ largestnumber.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of a: ");
+	scanf("%d",&a);
+	printf("Enter the value of b: ");
+	scanf("%d",&b);
+	printf("Enter the value of c: ");
+	scanf("%d",&c);
+	
+	if(a>b && a>c)
+	{
+		printf("%d is the largest number\n",a);
+	}
+	else if(b>a &&b>c)
+	{
+		printf("%d is the largest number\n",b);
+	}
+	else
+	{
+		printf("%d is the largest number",c);
+	}
+
+	
+	return 0;
+}

7a87ef2f668c88b88eb70da0b7f5b0a23799c1f5
Vaibhav Raj <Vaibhavraj049@gmail.com>
2024-10-02 00:52:02 +0530

    Add files via upload

---
+++ sizeofintcharfloat.c
@@ -0,0 +1,11 @@
+#include<Stdio.h>
+int main()
+{
+	printf("size of int: %d bytes\n", sizeof(int));
+	printf("size of char: %d bytes\n", sizeof(char));
+	printf("size of float: %d bytes\n", sizeof(float));
+	printf("size of double: %d bytes\n", sizeof(double));
+	printf("size of short int: %d bytes\n", sizeof(short int));
+	
+	return 0;
+}
